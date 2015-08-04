49bb710632b9fd3152052d2b9fae81a7320697ed
Pranav Lakshminarayanan <pranz.laks@gmail.com>
2015-08-04 13:54:32 -0400

    Revert "First commit"
    This reverts commit 4bd96f0600fc61d34e1ca30dbb35c24240d2b82f.

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Pranav\Documents\NetBeansProjects\ARToolkit\EclipseProjects\ARBaseLib\bin\AndroidManifest.xml : \
-C:\Users\Pranav\Documents\NetBeansProjects\ARToolkit\EclipseProjects\ARBaseLib\AndroidManifest.xml \
